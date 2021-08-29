a191980da4c05b4b00ec5c4ab270f14b65b0b84e
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-08-29 12:20:55 -0300

    Created front-web

---
+++ front-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MEU PRIMEIRO APP</h1>
+    </div>
+  );
+}
+
+export default App;
